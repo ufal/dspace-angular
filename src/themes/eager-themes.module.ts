bb43345bf7c139f4351eed35a7edd85d7d26e738
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2024-11-11 12:59:13 +0100

    update based on docs
    https://wiki.lyrasis.org/display/DSDOC7x/User+Interface+Customization
    > 4. (As of 7.3 or above) Import the new theme's eager-theme.module.ts
    in themes/eager-themes.module.ts. If you're switching from one theme to
    another, remove the old theme from the imports. Below is an example for
    a theme named "my-theme":

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+import { EagerThemeModule as UKRepoEagerThemeModule } from './ukrepo/eager-theme.module';
+// import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 
 /**
@@ -11,7 +12,8 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
  */
 @NgModule({
   imports: [
-    DSpaceEagerThemeModule,
+          UKRepoEagerThemeModule,
+    // DSpaceEagerThemeModule,
     // CustomEagerThemeModule,
   ],
 })
