c820f30f3ff25878300af17598f3d180cc6fc80d
Marvin <linosnchena@gmail.com>
2024-12-04 12:30:38 +0100

    lint fixations

--- src/app/handle-page/handle-table/handle-table.component.ts
+++ src/app/handle-page/handle-table/handle-table.component.ts
@@ -1,22 +1,43 @@
-import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { BehaviorSubject, combineLatest as observableCombineLatest, fromEvent } from 'rxjs';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  BehaviorSubject,
+  combineLatest as observableCombineLatest,
+  fromEvent,
+} from 'rxjs';
 import { RemoteData } from '../../core/data/remote-data';
 import { PaginatedList } from '../../core/data/paginated-list.model';
 import { HandleDataService } from '../../core/data/handle-data.service';
 import { PaginationComponentOptions } from '../../shared/pagination/pagination-component-options.model';
-import { debounceTime, distinctUntilChanged, switchMap, take } from 'rxjs/operators';
-import { getFirstSucceededRemoteData, getRemoteDataPayload } from '../../core/shared/operators';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  take,
+} from 'rxjs/operators';
+import {
+  getFirstSucceededRemoteData,
+  getRemoteDataPayload,
+} from '../../core/shared/operators';
 import { PaginationService } from '../../core/pagination/pagination.service';
 import {
   getHandleTableModulePath,
   HANDLE_TABLE_EDIT_HANDLE_PATH,
-  HANDLE_TABLE_NEW_HANDLE_PATH
+  HANDLE_TABLE_NEW_HANDLE_PATH,
 } from '../handle-page-routing-paths';
 import { isEmpty, isNotEmpty } from '../../shared/empty.util';
 import { Router } from '@angular/router';
 import { DeleteRequest } from '../../core/data/request.models';
 import { RequestService } from '../../core/data/request.service';
-import { defaultPagination, defaultSortConfiguration } from './handle-table-pagination';
+import {
+  defaultPagination,
+  defaultSortConfiguration,
+} from './handle-table-pagination';
 import { TranslateService } from '@ngx-translate/core';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
 import { SortOptions } from '../../core/cache/models/sort-options.model';
@@ -26,10 +47,9 @@ import {
   COMMUNITY,
   ITEM,
   SITE,
-  SUCCESSFUL_RESPONSE_START_CHAR
+  SUCCESSFUL_RESPONSE_START_CHAR,
 } from '../../core/handle/handle.resource-type';
 import { getHandlePageRoute } from 'src/app/community-page/community-page-routing-paths';
-
 
 /**
  * Constants for converting the searchQuery for the server
@@ -44,22 +64,18 @@ export const RESOURCE_TYPE_SEARCH_OPTION = 'resourceTypeId';
 @Component({
   selector: 'ds-handle-table',
   templateUrl: './handle-table.component.html',
-  styleUrls: ['./handle-table.component.scss']
+  styleUrls: ['./handle-table.component.scss'],
 })
 export class HandleTableComponent implements OnInit {
-
-  getHandlePageRoute(resourceId: string | undefined): string {
-    return getHandlePageRoute(resourceId ?? '');
-  }
-
-  constructor(private handleDataService: HandleDataService,
+  constructor(
+    private handleDataService: HandleDataService,
     private paginationService: PaginationService,
     public router: Router,
     private requestService: RequestService,
     private cdr: ChangeDetectorRef,
     private translateService: TranslateService,
-    private notificationsService: NotificationsService,) {
-  }
+    private notificationsService: NotificationsService
+  ) {}
 
   /**
    * The reference for the input html element
@@ -69,7 +85,8 @@ export class HandleTableComponent implements OnInit {
   /**
    * The list of Handle object as BehaviorSubject object
    */
-  handlesRD$: BehaviorSubject<RemoteData<PaginatedList<Handle>>> = new BehaviorSubject<RemoteData<PaginatedList<Handle>>>(null);
+  handlesRD$: BehaviorSubject<RemoteData<PaginatedList<Handle>>> =
+    new BehaviorSubject<RemoteData<PaginatedList<Handle>>>(null);
 
   /**
    * The amount of versions to display per page
@@ -137,45 +154,67 @@ export class HandleTableComponent implements OnInit {
    */
   selectedHandle = null;
 
-
   ngOnInit(): void {
     this.handleRoute = getHandleTableModulePath();
     this.initializePaginationOptions();
     this.initializeSortingOptions();
     this.getAllHandles();
 
-    this.handleOption = this.translateService.instant('handle-table.table.handle');
-    this.internalOption = this.translateService.instant('handle-table.table.internal');
-    this.resourceTypeOption = this.translateService.instant('handle-table.table.resource-type');
+    this.handleOption = this.translateService.instant(
+      'handle-table.table.handle'
+    );
+    this.internalOption = this.translateService.instant(
+      'handle-table.table.internal'
+    );
+    this.resourceTypeOption = this.translateService.instant(
+      'handle-table.table.resource-type'
+    );
   }
 
   /**
    * Load all handles based on the pagination and sorting options.
    */
   getAllHandles() {
-    this.handlesRD$ = new BehaviorSubject<RemoteData<PaginatedList<Handle>>>(null);
+    this.handlesRD$ = new BehaviorSubject<RemoteData<PaginatedList<Handle>>>(
+      null
+    );
     this.isLoading = true;
 
     // load the current pagination and sorting options
-    const currentPagination$ = this.paginationService.getCurrentPagination(this.options.id, this.options);
-    const currentSort$ = this.paginationService.getCurrentSort(this.options.id, this.sortConfiguration);
+    const currentPagination$ = this.paginationService.getCurrentPagination(
+      this.options.id,
+      this.options
+    );
+    const currentSort$ = this.paginationService.getCurrentSort(
+      this.options.id,
+      this.sortConfiguration
+    );
 
-    observableCombineLatest([currentPagination$, currentSort$]).pipe(
-      switchMap(([currentPagination, currentSort]) => {
-        return this.handleDataService.findAll({
-          currentPage: currentPagination.currentPage,
-          elementsPerPage: currentPagination.pageSize,
-          sort: { field: currentSort.field, direction: currentSort.direction }
-        }, false
-        );
-      }),
-      getFirstSucceededRemoteData()
-    ).subscribe((res: RemoteData<PaginatedList<Handle>>) => {
-      this.handlesRD$.next(res);
-      this.isLoading = false;
-    });
+    observableCombineLatest([currentPagination$, currentSort$])
+      .pipe(
+        switchMap(([currentPagination, currentSort]) => {
+          return this.handleDataService.findAll(
+            {
+              currentPage: currentPagination.currentPage,
+              elementsPerPage: currentPagination.pageSize,
+              sort: {
+                field: currentSort.field,
+                direction: currentSort.direction,
+              },
+            },
+            false
+          );
+        }),
+        getFirstSucceededRemoteData()
+      )
+      .subscribe((res: RemoteData<PaginatedList<Handle>>) => {
+        this.handlesRD$.next(res);
+        this.isLoading = false;
+      });
+  }
 
-
+  getHandlePageRoute(resourceId: string | undefined): string {
+    return getHandlePageRoute(resourceId ?? '');
   }
 
   /**
@@ -201,9 +240,9 @@ export class HandleTableComponent implements OnInit {
    * Redirect to the new handle component with the current pagination options.
    */
   redirectWithCurrentPage() {
-    this.router.navigate([this.handleRoute, this.newHandlePath],
-      { queryParams: { currentPage: this.options.currentPage } },
-    );
+    this.router.navigate([this.handleRoute, this.newHandlePath], {
+      queryParams: { currentPage: this.options.currentPage },
+    });
   }
 
   /**
@@ -215,26 +254,30 @@ export class HandleTableComponent implements OnInit {
       return;
     }
 
-    this.handlesRD$.pipe(
-      // take just one value from subscription because if is the subscription active this code runs after every
-      // this.handleRD$ update
-      take(1)
-    ).subscribe((handleRD) => {
-      handleRD.payload.page.forEach(handle => {
-        if (handle.id === this.selectedHandle) {
-          this.switchSelectedHandle(this.selectedHandle);
-          this.router.navigate([this.handleRoute, this.editHandlePath],
-            {
+    this.handlesRD$
+      .pipe(
+        // take just one value from subscription because if is the subscription active this code runs after every
+        // this.handleRD$ update
+        take(1)
+      )
+      .subscribe((handleRD) => {
+        handleRD.payload.page.forEach((handle) => {
+          if (handle.id === this.selectedHandle) {
+            this.switchSelectedHandle(this.selectedHandle);
+            this.router.navigate([this.handleRoute, this.editHandlePath], {
               queryParams: {
-                id: handle.id, _selflink: handle._links.self.href, handle: handle.handle,
-                url: handle.url, resourceType: handle.resourceTypeID, resourceId: handle.id,
-                currentPage: this.options.currentPage
-              }
-            },
-          );
-        }
+                id: handle.id,
+                _selflink: handle._links.self.href,
+                handle: handle.handle,
+                url: handle.url,
+                resourceType: handle.resourceTypeID,
+                resourceId: handle.id,
+                currentPage: this.options.currentPage,
+              },
+            });
+          }
+        });
       });
-    });
   }
 
   /**
@@ -248,47 +291,65 @@ export class HandleTableComponent implements OnInit {
 
     let requestId = '';
     // delete handle
-    this.handlesRD$.pipe(
-      // take just one value from subscription because if is the subscription active this code runs after every
-      // this.handleRD$ update
-      take(1)
-    ).subscribe((handleRD) => {
-      handleRD.payload.page.forEach(handle => {
-        if (handle.id === this.selectedHandle) {
-          requestId = this.requestService.generateRequestId();
-          const deleteRequest = new DeleteRequest(requestId, handle._links.self.href);
-          // call delete request
-          this.requestService.send(deleteRequest);
-          // unselect deleted handle
-          this.refreshTableAfterDelete(handle.id);
-        }
+    this.handlesRD$
+      .pipe(
+        // take just one value from subscription because if is the subscription active this code runs after every
+        // this.handleRD$ update
+        take(1)
+      )
+      .subscribe((handleRD) => {
+        handleRD.payload.page.forEach((handle) => {
+          if (handle.id === this.selectedHandle) {
+            requestId = this.requestService.generateRequestId();
+            const deleteRequest = new DeleteRequest(
+              requestId,
+              handle._links.self.href
+            );
+            // call delete request
+            this.requestService.send(deleteRequest);
+            // unselect deleted handle
+            this.refreshTableAfterDelete(handle.id);
+          }
+        });
       });
-    });
 
     // check response
-    this.requestService.getByUUID(requestId)
-      .subscribe(info => {
-        // if is empty
-        if (!isNotEmpty(info) || !isNotEmpty(info.response) || !isNotEmpty(info.response.statusCode)) {
-          // do nothing - in another subscription should be data
-          return;
-        }
+    this.requestService.getByUUID(requestId).subscribe((info) => {
+      // if is empty
+      if (
+        !isNotEmpty(info) ||
+        !isNotEmpty(info.response) ||
+        !isNotEmpty(info.response.statusCode)
+      ) {
+        // do nothing - in another subscription should be data
+        return;
+      }
 
-        if (info.response.statusCode.toString().startsWith(SUCCESSFUL_RESPONSE_START_CHAR)) {
-          this.notificationsService.success(null, this.translateService.get('handle-table.delete-handle.notify.successful'));
-        } else {
-          // write error in the notification
-          // compose error message with message definition and server error
-          let errorMessage = '';
-          this.translateService.get('handle-table.delete-handle.notify.error').pipe(
-            take(1)
-          ).subscribe(message => {
+      if (
+        info.response.statusCode
+          .toString()
+          .startsWith(SUCCESSFUL_RESPONSE_START_CHAR)
+      ) {
+        this.notificationsService.success(
+          null,
+          this.translateService.get(
+            'handle-table.delete-handle.notify.successful'
+          )
+        );
+      } else {
+        // write error in the notification
+        // compose error message with message definition and server error
+        let errorMessage = '';
+        this.translateService
+          .get('handle-table.delete-handle.notify.error')
+          .pipe(take(1))
+          .subscribe((message) => {
             errorMessage = message + ': ' + info.response.errorMessage;
           });
 
-          this.notificationsService.error(null, errorMessage);
-        }
-      });
+        this.notificationsService.error(null, errorMessage);
+      }
+    });
   }
 
   /**
@@ -306,27 +367,29 @@ export class HandleTableComponent implements OnInit {
     const interval = setInterval(() => {
       let isHandleInTable = false;
       // Load handle from the DB
-      this.handleDataService.findAll({
-        currentPage: this.options.currentPage,
-        elementsPerPage: this.options.pageSize,
-      }, false
-      ).pipe(
-        getFirstSucceededRemoteData(),
-        getRemoteDataPayload()
-      ).subscribe(handles => {
-        // check if the handle is in the table data
-        if (handles.page.some(handle => handle.id === deletedHandleId)) {
-          isHandleInTable = true;
-        }
+      this.handleDataService
+        .findAll(
+          {
+            currentPage: this.options.currentPage,
+            elementsPerPage: this.options.pageSize,
+          },
+          false
+        )
+        .pipe(getFirstSucceededRemoteData(), getRemoteDataPayload())
+        .subscribe((handles) => {
+          // check if the handle is in the table data
+          if (handles.page.some((handle) => handle.id === deletedHandleId)) {
+            isHandleInTable = true;
+          }
 
-        // reload table if the handle was removed from the database
-        if (!isHandleInTable) {
-          this.switchSelectedHandle(deletedHandleId);
-          this.getAllHandles();
-          this.cdr.detectChanges();
-          clearInterval(interval);
-        }
-      });
+          // reload table if the handle was removed from the database
+          if (!isHandleInTable) {
+            this.switchSelectedHandle(deletedHandleId);
+            this.getAllHandles();
+            this.cdr.detectChanges();
+            clearInterval(interval);
+          }
+        });
 
       // Clear interval after 20s timeout
       if (counter === (refreshTimeout * 1000) / 250) {
@@ -347,18 +410,14 @@ export class HandleTableComponent implements OnInit {
     }
 
     fromEvent(this.searchInput.nativeElement, 'keyup')
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged()
-      )
-      .subscribe(cc => {
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((cc) => {
         this.searchHandles(this.searchInput.nativeElement.value);
         setTimeout(() => {
           // click to refresh table data because without click it still shows wrong data
           document.getElementById('clarin-dc-search-box').click();
         }, 25);
       });
-
   }
 
   /**
