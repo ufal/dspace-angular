0164d08e6ebf4b672ce8ec72860886fa40b991df
amadulhaxxani <hassan@ufal.mff.cuni.cz>
2026-03-04 14:45:55 +0100

    Clarify default allowlisted markdown fields
    Clarify default allowlisted markdown fields

--- src/config/default-app-config.ts
+++ src/config/default-app-config.ts
@@ -409,7 +409,8 @@ export class DefaultAppConfig implements AppConfig {
   };
 
   // Whether to enable Markdown (https://commonmark.org/) and MathJax (https://www.mathjax.org/)
-  // display in supported metadata fields. By default, only dc.description.abstract is supported.
+  // display in supported metadata fields. By default, allowlisted description fields
+  // (description, dc.description, dc.description.abstract) are supported.
   markdown: MarkdownConfig = {
     enabled: false,
     mathjax: false
