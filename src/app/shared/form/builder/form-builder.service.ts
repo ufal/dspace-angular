e0881fa1759c6671a162c7bb999c57ca303136a1
amadulhaxxani <hassan@ufal.mff.cuni.cz>
2026-03-26 14:04:25 +0100

    Simplify array index presence check
    Simplify array index presence check

--- src/app/shared/form/builder/form-builder.service.ts
+++ src/app/shared/form/builder/form-builder.service.ts
@@ -244,7 +244,7 @@ export class FormBuilderService extends DynamicFormService {
         return new FormFieldMetadataValueObject(dateToString(controlValue));
       } else if (isObject(controlValue)) {
         const authority = (controlValue as any).authority || (controlValue as any).id || null;
-        const hasArrayIndex = controlModelIndex !== undefined && controlModelIndex !== null;
+        const hasArrayIndex = controlModelIndex !== null;
         const place = hasArrayIndex ? controlModelIndex : (controlValue as any).place;
         if (isNgbDateStruct(controlValue)) {
           return new FormFieldMetadataValueObject(controlValue, controlLanguage, authority, controlValue as any, place);
