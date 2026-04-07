d380240c9139ff8d7a473dbe9a7706fcc11aab09
amadulhaxxani <hassan@ufal.mff.cuni.cz>
2026-04-07 16:33:33 +0200

    Use shareReplay to cache version draft observable
    Import shareReplay and apply .pipe(shareReplay(1)) to the version draft observable in ItemActionsComponent. This shares the subscription and caches the latest emission so multiple subscribers won't trigger duplicate work or requests.

--- src/app/shared/mydspace-actions/item/item-actions.component.ts
+++ src/app/shared/mydspace-actions/item/item-actions.component.ts
@@ -2,6 +2,7 @@ import { Component, Injector, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable, of as observableOf } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { MyDSpaceActionsComponent } from '../mydspace-actions';
 import { ItemDataService } from '../../../core/data/item-data.service';
 import { Item } from '../../../core/shared/item.model';
@@ -115,7 +116,7 @@ export class ItemActionsComponent extends MyDSpaceActionsComponent<Item, ItemDat
       this.object,
       'item.page.version.hasDraft',
       'item.page.version.create',
-    );
+    ).pipe(shareReplay(1));
   }
 
   /**
