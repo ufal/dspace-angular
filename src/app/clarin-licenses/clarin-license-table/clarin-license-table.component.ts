ec706748ab736a20d793df97354475b7e4d69264
amadulhaxxani <hassan@ufal.mff.cuni.cz>
2026-04-28 14:04:03 +0200

    Use hasNoValue for license id check
    Replace isNull with hasNoValue when validating selectedLicense.id in deleteLicense to correctly handle undefined/null values. Also add hasNoValue to the imports from shared/empty.util.

--- src/app/clarin-licenses/clarin-license-table/clarin-license-table.component.ts
+++ src/app/clarin-licenses/clarin-license-table/clarin-license-table.component.ts
@@ -15,7 +15,7 @@ import { DefineLicenseLabelFormComponent } from './modal/define-license-label-fo
 import { ClarinLicenseConfirmationSerializer } from '../../core/shared/clarin/clarin-license-confirmation-serializer';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
 import { TranslateService } from '@ngx-translate/core';
-import { isNull } from '../../shared/empty.util';
+import { hasNoValue, isNull } from '../../shared/empty.util';
 import { ClarinLicenseLabel } from '../../core/shared/clarin/clarin-license-label.model';
 import { ClarinLicenseLabelDataService } from '../../core/data/clarin/clarin-license-label-data.service';
 import { ClarinLicenseLabelExtendedSerializer } from '../../core/shared/clarin/clarin-license-label-extended-serializer';
@@ -274,7 +274,7 @@ export class ClarinLicenseTableComponent implements OnInit {
    * Delete selected license. If none license is selected do nothing.
    */
   deleteLicense() {
-    if (isNull(this.selectedLicense?.id) || this.isSelectedLicenseInUse()) {
+    if (hasNoValue(this.selectedLicense?.id) || this.isSelectedLicenseInUse()) {
       return;
     }
     this.clarinLicenseService.delete(String(this.selectedLicense.id))
