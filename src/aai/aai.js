fe3846d31b1e7489daa1e2b0e07a09a76daef073
amadulhaxxani <hassan@ufal.mff.cuni.cz>
2026-05-11 11:30:50 +0200

    reverting accidental changes
    reverting accidental changes

--- src/aai/aai.js
+++ src/aai/aai.js
@@ -3,8 +3,7 @@
   function AAI() {
     var host = 'https://' + window.location.hostname,
         ourEntityID = host.match("lindat.mff.cuni.cz") ? "https://ufal-point.mff.cuni.cz" : host;
-    // var namespace = 'repository';
-    var namespace = '';
+    var namespace = 'repository';
     this.defaults = {
       //host : 'https://ufal-point.mff.cuni.cz',
       host : host, //better default (useful when testing on ufal-point-dev)
