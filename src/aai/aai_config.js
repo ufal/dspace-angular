d878d2ab2322cd648e4287956fae79617b882aca
Milan Kuchtiak <kuchtiak@ufal.mff.cuni.cz>
2026-09-02 11:48:35 +0200

    NRP k8s demo: allow local authentication only

--- src/aai/aai_config.js
+++ src/aai/aai_config.js
@@ -35,6 +35,7 @@ jQuery(document).ready(
                     '<p  style="" ><input type="submit" style="margin: 20px 2px" name="submit" value="Sign in" /></p>' +
                     '</form>';
             instance.target = instance.target + "authn/shibboleth";
+            instance.autoInitialize = false;
             return instance;
         })();
         if (!("aai" in window)) {
